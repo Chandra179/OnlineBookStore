4fd0118461d1feb55697a2fa70793385cbdef3e6
Chandra178_ <chandra@mplus.software>
2021-10-10 16:20:58 +0700

    add serializer for book & bookAuthor

--- core/books/serializers.py
+++ core/books/serializers.py
@@ -1,8 +1,16 @@
 from rest_framework import serializers
-from .models import BookAuthor
+from .models import Book, BookAuthor
   
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = ('title',)
+        depth = 1
+
 
 class BookAuthorSerializer(serializers.ModelSerializer):
     class Meta:
         model = BookAuthor
         fields = '__all__'
+        depth = 1
