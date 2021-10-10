cbc122742a9462c07bc9eb8910f2419d0596d429
Chandra178_ <chandra@mplus.software>
2021-10-10 16:21:42 +0700

    add books views

--- core/books/views.py
+++ core/books/views.py
@@ -1,8 +1,15 @@
-from rest_framework import viewsets
-from .serializers import BookAuthorSerializer
-from .models import Book, BookAuthor
+from rest_framework import viewsets, generics
+from .serializers import BookSerializer
+from .models import Book
 
 
-class BookAuthorView(viewsets.ModelViewSet):
-    queryset = BookAuthor.objects.all()
-    serializer_class = BookAuthorSerializer
+class BookView(viewsets.ModelViewSet):
+    serializer_class = BookSerializer
+
+    def get_queryset(self):
+        queryset = Book.objects.all()
+        title = self.request.query_params.get('title')
+        if title is not None:
+            queryset = queryset.filter(book__title=title, )
+        return queryset
+    
