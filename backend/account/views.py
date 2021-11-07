ff4a39114df18221380d535b6b3f282c8ef47bdd
Chandra178_ <chandra@mplus.software>
2021-11-07 14:17:04 +0700

    add login api to backend

--- backend/account/views.py
+++ backend/account/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import authentication, permissions
+from .models import Book
 
-# Create your views here.
+class Login(APIView):
+    
+    def get(self, request, format=None):
+        """
+        Return a list of all users.
+        """
+        book_title = [book.title for book in Book.objects.all()]
+        return Response(book_title)
