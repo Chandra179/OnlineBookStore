9f5766fde47fb548c0b2ac2ccf889aee7a85768a
Chandra178_ <chandra@mplus.software>
2021-11-09 22:12:13 +0700

    move authentication logout to buttonappbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Switch, Route, Link } from "react-router-dom";
-import "./App.css";
-
 import AuthService from "./services/auth.service";
-import Login from "./components/Login";
 import ButtonAppBar from "./components/ButtonAppBar"
 
 const App = () => {
@@ -15,40 +12,10 @@ const App = () => {
       setCurrentUser(user);
     }
   }, []);
-
-  const logOut = () => {
-    AuthService.logout();
-  };
-
+  
   return (
     <div>
         <ButtonAppBar isLoggedin={currentUser} />
-        {currentUser ? (
-          <div>
-            <li>
-                {currentUser}
-            </li>
-            <li>
-              <a href="/login" className="nav-link" onClick={logOut}>
-                LogOut
-              </a>
-            </li>
-          </div>
-        ) : (
-          <div>
-            <li>
-              <Link to={"/login"} className="nav-link">
-                Login
-              </Link>
-            </li>
-          </div>
-        )}
-
-      <div className="container mt-3">
-        <Switch>
-          <Route exact path="/login" component={Login} />
-        </Switch>
-      </div>
     </div>
   );
 };
--- frontend/src/components/ButtonAppBar.js
+++ frontend/src/components/ButtonAppBar.js
@@ -6,8 +6,17 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import { Switch, Route, Link } from "react-router-dom";
+import Login from "./Login";
+import AuthService from "../services/auth.service";
+import styles from "../static/css/ButtonAppBar.module.css"
 
 const ButtonAppBar = ({ isLoggedin }) => {
+
+    const logOut = () => {
+        AuthService.logout();
+    };
+
     return (
         <Box sx={{ flexGrow: 1 }}>
             <AppBar position="static">
@@ -25,40 +34,24 @@ const ButtonAppBar = ({ isLoggedin }) => {
                         News
                     </Typography>
                     {isLoggedin ? (
-                        <Button color="inherit">Logout</Button>
+                        <a href="/login" onClick={logOut}>
+                            <Button color="primary">
+                                LogOut
+                            </Button>
+                        </a>
                     ) : (
-
-                        <Button color="inherit">Login</Button>
+                        <Link to={"/login"}>
+                            <Button color="inherit">
+                                Login
+                            </Button>
+                        </Link>
                     )}
                 </Toolbar>
             </AppBar>
+            <Switch>
+                <Route exact path="/login" component={Login} />
+            </Switch>
         </Box>
     )
 };
 export default ButtonAppBar
-
-// export default function ButtonAppBar() {
-//     const [isLoggedin, setIsloggedin] = useState(false);
-
-//     return (
-//         <Box sx={{ flexGrow: 1 }}>
-//             <AppBar position="static">
-//                 <Toolbar>
-//                     <IconButton
-//                         size="large"
-//                         edge="start"
-//                         color="inherit"
-//                         aria-label="menu"
-//                         sx={{ mr: 2 }}
-//                     >
-//                         <MenuIcon />
-//                     </IconButton>
-//                     <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-//                         News
-//                     </Typography>
-//                     <Button color="inherit">Login</Button>
-//                 </Toolbar>
-//             </AppBar>
-//         </Box>
-//     );
-// }
