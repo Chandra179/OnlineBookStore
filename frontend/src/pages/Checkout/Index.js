bc16472da3e739f7b71250594d974ac0690fb831
Chandra179 <chandrafirst67@gmail.com>
2022-02-16 19:51:55 +0700

    add checkout

---
+++ frontend/src/pages/Checkout/Index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Grid from "@mui/material/Grid";
+import { Typography } from "@mui/material";
+import Divider from "@mui/material/Divider";
+import Categories from "../components/Categories";
+
+function Checkout() {
+  return (
+      <>
+      </>
+  );
+}
+
+export default Checkout;
