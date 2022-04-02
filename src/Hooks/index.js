1825fba534dda10c5955052a605ab8a4c8e30e5b
Chandra178_ <chandra@mplus.software>
2022-04-02 11:49:21 +0700

    'add'

--- src/Hooks/index.js
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react";
-import { CartContext } from "../Context/cart-context";
-
-export const useCart = () => {
-  const carttx = useContext(CartContext);
-  return {
-    ...carttx,
-  };
-};
