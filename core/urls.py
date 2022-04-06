1f205fc127bfb680bcf871426a73a2fc5859cd38
Chandra178_ <chandra@mplus.software>
2022-04-06 14:01:43 +0700

    'add'

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
 from rest_framework.authtoken import views
-# from .views import index
+from .views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,11 +11,11 @@ urlpatterns = [
     path('order', include('order.urls')),
     
     # REACT ROUTER-DJANGO
-    # re_path(r'^(%s)?$' % '|', index, name='index'),
-    # path('genres/<str:genre>/<int:page>',index),
-    # path('genres/<str:genre>/<int:page>/<str:title>',index),
-    # path('cart',index),
-    # path('signin',index),
-    # path('signup',index),
-    # path('checkout',index),
+    re_path(r'^(%s)?$' % '|', index, name='index'),
+    path('genres/<str:genre>/<int:page>', index),
+    path('genres/<str:genre>/<int:page>/<str:title>', index),
+    path('cart', index),
+    path('signin', index),
+    path('signup', index),
+    path('checkout', index),
 ]
